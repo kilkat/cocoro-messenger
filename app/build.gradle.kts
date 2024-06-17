8df2fbdfbfd1686fee823ce85bceeed8b1bcd6aa
kilkat <rhkddns3459@naver.com>
2024-06-18 02:02:42 +0900

    (24-0618)contact_intent_디자인_개선

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    implementation ("com.squareup.retrofit2:converter-scalars:2.9.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation ("com.google.android.material:material:1.12.0")
 }
